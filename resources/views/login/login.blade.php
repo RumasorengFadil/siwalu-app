8c7e593b46a5da04b8c553168f06a05e7cb25500
Fadil Hijayat <limitvariabel@gmail.com>
2024-04-07 11:40:37 +0900

    register and login page completed

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -10,9 +10,57 @@
 <body>
     <header>
         <nav class="nav">
-            <img class="nav__logo" src="/img/logo.svg" alt="">
-            <h1 class="nav__title">Siwalu</h1>
+            <a href="{{url("/")}}">
+                <img class="nav__logo" src="/img/logo.svg" alt="">
+            </a>
+            <h1 class="nav__brand-name">Siwalu</h1>
         </nav>
     </header>
+
+    <main class="register-main">
+        <a  href="{{url('/')}}"><img class="register-main__back-icon" src="icn/chevron-left.svg" alt=""></a>
+        <form class="form" action="" method="post">
+            <input class="form__input-email" type="email" name="" id="" placeholder="Email"><br>
+            <input class="form__input-pass" type="password" name="" id="" placeholder="Password"><br>
+            <input class="form__submit-btn" type="submit" value="Login">
+
+            <p class="form__link">Belum punya akun? <a href="{{url('register')}}">daftar yuk!</a></p>
+        </form>
+    </main>
+
+    <footer class="footer">
+        <div class="footer__content">
+            <div class="footer__about-brand">
+                <div class="footer__logo">
+                    <img class="nav__logo" src="/img/logo-white.svg" alt="">
+                    <h1 class="nav__brand-name footer__brand-name">Siwalu</h1>
+                </div>
+                
+                <p class="footer__about-brand-text">This apps is bla-bla yang panjanganya bisa sampe 2-3 baris sih jadi ntar isi sama highlight appsnya mau apa</p>
+            </div>
+
+            <div class="footer__quick-links">
+                <h4 class="footer__title">Quick Links</h4>
+                <ul class="footer__links">
+                    <a href="{{url("/")}}" class="footer__link">Home</a>
+                    <a href="" class="footer__link">About Company</a>
+                    <a href="" class="footer__link">Partner</a>
+                    <a href="" class="footer__link">Terms & Condition</a>
+                </ul>
+            </div>
+
+            <div class="footer__contact-us">
+                <h4 class="footer__title">Contact Us</h4>
+                <p class="footer__address">Jl. Berdua bersamamu, Kec. Dalam,
+                    Kota Mimpi, Yang Pernah Indah 40122</p>
+                <p class="footer__phone-number">+62 8515-5880-6466</p>
+                <p class="footer__email">contact@apps.com</p>
+            </div>
+        </div>
+
+        <div class="footer__copyright">
+            <p class="footer__copyright-text">Copyright Apps @ 2023. All rights reserved.</p>
+        </div>
+    </footer>
 </body>
 </html>
--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -9,25 +9,58 @@
 <body>
     <header>
         <nav class="nav">
-            <img class="nav__logo" src="/img/logo.svg" alt="">
-            <h1 class="nav__title">Siwalu</h1>
+            <a href="{{url("/")}}">
+                <img class="nav__logo" src="/img/logo.svg" alt="">
+            </a>
+            <h1 class="nav__brand-name">Siwalu</h1>
         </nav>
     </header>
 
     <main class="register-main">
         <a  href="{{url('/')}}"><img class="register-main__back-icon" src="icn/chevron-left.svg" alt=""></a>
-        <form class="form-register" action="" method="post">
-            <input class="form-register__input-email" type="email" name="" id="" placeholder="Email"><br>
-            <input class="form-register__input-pass" type="password" name="" id="" placeholder="Password"><br>
-            <input class="form-register__regist-btn" type="submit" value="Daftar">
+        <form class="form" action="" method="post">
+            <input class="form__input-email" type="email" name="" id="" placeholder="Email"><br>
+            <input class="form__input-pass" type="password" name="" id="" placeholder="Password"><br>
+            <input class="form__submit-btn" type="submit" value="Daftar">
 
-            <p class="form-register__privacy-policy">Dengan membuat akun kamu telah menyetujui <a href="">Syarat & Ketentuan</a> dan <a href="">Kebijakan Privasi</a></p>
-            <p class="form-register__login-btn">Udah punya akun? <a href="{{url('login')}}">masuk disini!</a></p>
+            <p class="form__privacy-policy">Dengan membuat akun kamu telah menyetujui <a href="">Syarat & Ketentuan</a> dan <a href="">Kebijakan Privasi</a></p>
+            <p class="form__link">Udah punya akun? <a href="{{url('login')}}">masuk disini!</a></p>
         </form>
     </main>
 
-    <footer>
-        
+    <footer class="footer">
+        <div class="footer__content">
+            <div class="footer__about-brand">
+                <div class="footer__logo">
+                    <img class="nav__logo" src="/img/logo-white.svg" alt="">
+                    <h1 class="nav__brand-name footer__brand-name">Siwalu</h1>
+                </div>
+                
+                <p class="footer__about-brand-text">This apps is bla-bla yang panjanganya bisa sampe 2-3 baris sih jadi ntar isi sama highlight appsnya mau apa</p>
+            </div>
+
+            <div class="footer__quick-links">
+                <h4 class="footer__title">Quick Links</h4>
+                <ul class="footer__links">
+                    <a href="{{url("/")}}" class="footer__link">Home</a>
+                    <a href="" class="footer__link">About Company</a>
+                    <a href="" class="footer__link">Partner</a>
+                    <a href="" class="footer__link">Terms & Condition</a>
+                </ul>
+            </div>
+
+            <div class="footer__contact-us">
+                <h4 class="footer__title">Contact Us</h4>
+                <p class="footer__address">Jl. Berdua bersamamu, Kec. Dalam,
+                    Kota Mimpi, Yang Pernah Indah 40122</p>
+                <p class="footer__phone-number">+62 8515-5880-6466</p>
+                <p class="footer__email">contact@apps.com</p>
+            </div>
+        </div>
+
+        <div class="footer__copyright">
+            <p class="footer__copyright-text">Copyright Apps @ 2023. All rights reserved.</p>
+        </div>
     </footer>
 </body>
 </html>
